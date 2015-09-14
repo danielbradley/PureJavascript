d917f0826db41ee69d07809f9349ccab525441f1
Daniel Bradley <daniel@webelegant.com>
2015-09-14 17:33:43 +1000

    Reorganised repository.

--- latest/purejavascript/resources/javascript/purejavascript/SetCookie.js
+++
@@ -1,9 +0,0 @@
-
-function SetCookie( path, cname, cvalue, exdays )
-{
-    var d       = new Date(); d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+d.toUTCString();
-	var cookie  = cname + "=" + cvalue + "; " + expires + "; " + "path=" + path + ";";
-	
-    document.cookie = cookie;
-}
