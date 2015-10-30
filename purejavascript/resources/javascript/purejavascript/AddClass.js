bc2041f10ac03b56190f67d205e5188c8b2f6c35
Andrew McRae <andrew@webelegant.com>
2015-10-30 12:53:59 +1000

    Added new RemoveClass function and packaged related style functions in a StyleClasses object. Old function names are still valid.

--- purejavascript/resources/javascript/purejavascript/AddClass.js
+++ purejavascript/resources/javascript/purejavascript/AddClass.js
@@ -54,3 +54,49 @@ function( e, className )
 	}
 	return contains;
 }
+
+
+StyleClasses = {
+    //Append class if not already in classes list.
+    AddClass: AddClass,  //body remains outside StyleClasses for backwards compatibility.
+    
+    // Check if class is in element's classes.
+    ContainsClass:AddClass.Contains,
+    
+    //Remove a classname from an element's list of active styling classes.
+    RemoveClass: function(e, className) {
+        if ( className == e.className )
+        {
+            e.className='';
+            return true;
+        }
+        var maxStart = (e.className.length - className.length) - 1;
+        if ( 0 <= maxStart )
+        {
+            var start;
+            
+            start = e.className.indexOf( " " + className + " ");
+            if ( -1 != start )
+            {
+                e.className = e.className.substring(0,start) + e.className.substring(start+1+className.length);
+                return true;
+            }
+            
+            start = e.className.indexOf( className + " " );
+            if ( 0 == start)
+            {
+                e.className = e.className.substring(1+className.length);
+                return true;
+            }
+            
+            start = e.className.indexOf( " " + className );
+            if ( maxStart == start  )
+            {
+                e.className = e.className.substring(0, start);
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
